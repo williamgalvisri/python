3bfc662c5afaee7d26c8b6a1b569f1790847d1bc
williamgalvisri <92898916+williamgalvisri@users.noreply.github.com>
2022-04-29 13:27:42 -0500

    Delete python platzi directory

--- python platzi/for.py
+++
@@ -1,9 +0,0 @@
-def run():
-    for contador in range(1000):
-        if contador % 2 != 0:
-            continue
-        print(contador)
-
-# Punto de entrada de cualquien aplicación de python
-if __name__ == "__main__":
-    run()
--- python platzi/slice.py
+++
@@ -1,5 +0,0 @@
-nombre = "William"
-print(nombre[:3])
-
-# Nombre en reversa
-print(nombre[::-1])
